9d455ca27ef86ca758cb0277ff3483961d8a9f52
HBB20 <harshbbhakta@gmail.com>
2019-09-19 20:33:46 -0600

    - starts with View

--- countrypicker/src/main/java/com/hbb20/CountryPicker.kt
+++ countrypicker/src/main/java/com/hbb20/CountryPicker.kt
@@ -1,2 +1,11 @@
 package com.hbb20
 
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class CountryPicker(
+    context: Context?,
+    attrs: AttributeSet?,
+    defStyleAttr: Int
+) : ConstraintLayout(context, attrs, defStyleAttr)
