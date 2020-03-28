096fa61bc20110d00e68a3e6233e4c86d451c2f4
HBB20 <harshbbhakta@gmail.com>
2020-03-28 17:38:54 -0600

    - simplify code

--- app/src/main/java/com/hbb20/androidcountrypicker/CustomRecyclerViewActivity.kt
+++ app/src/main/java/com/hbb20/androidcountrypicker/CustomRecyclerViewActivity.kt
@@ -3,8 +3,6 @@ package com.hbb20.androidcountrypicker
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.hbb20.CPDataStoreGenerator
-import com.hbb20.countrypicker.CustomFlagImageProvider
 import com.hbb20.countrypicker.loadCountries
 import kotlinx.android.synthetic.main.activity_custom_recycler_view.*
 
@@ -18,46 +16,11 @@ class CustomRecyclerViewActivity : AppCompatActivity() {
 
 
     private fun setupRecyclerView() {
-        val dataStore = CPDataStoreGenerator.generate(resources)
-        //        val recyclerViewHelper = CPRecyclerViewHelper(
-        //            recyclerView,
-        //            dataStore,
-        //            cpRecyclerViewConfig = CPRecyclerViewConfig(
-        //                highlightedTextGenerator = { "+${it.phoneCode}" },
-        //                flagProvider = DefaultEmojiFlagProvider()
-        //            ),
-        //            onCountryClickListener = { cpCountry ->
-        //                Toast.makeText(
-        //                    this,
-        //                    "Selected ${cpCountry.englishName}",
-        //                    Toast.LENGTH_LONG
-        //                ).show()
-        //            },
-        //            queryEditText = etQuery
-        //        )
-
         recyclerView.loadCountries(
             resources = resources,
-            filterQueryEditText = etQuery,
-            highlightedTextGenerator = { country -> country.alpha2 },
-            customDataStoreModifier = { dataStore ->
-                dataStore.countryList.retainAll { it.currencyCode == "USD" }
-                dataStore.messageGroup.noMatchMsg = "Sorry, nothing is here"
-            },
-            flagProvider = MyFlagProvider()
+            filterQueryEditText = etQuery
         ) { selectedCountry ->
-            Toast.makeText(
-                this,
-                "Selected ${selectedCountry.englishName}",
-                Toast.LENGTH_LONG
-            ).show()
+            Toast.makeText(this, selectedCountry.name, Toast.LENGTH_SHORT).show()
         }
     }
-}
-
-class MyFlagProvider : CustomFlagImageProvider() {
-    override fun getFlag(alpha2Code: String): Int {
-        return R.drawable.ic_flag
-    }
-
 }
