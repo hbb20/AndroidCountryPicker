b2ef365fedd609beda4b26ae4866f8edb3e6b8ce
HBB20 <harshbbhakta@gmail.com>
2020-06-20 16:18:03 -0600

    - publishes single path

---
+++ AndroidCountryPicker/src/test/java/com/hbb20/countrypicker/CPRobolectricTestRunner.kt
@@ -0,0 +1,5 @@
+package com.hbb20.countrypicker
+
+import org.robolectric.RobolectricTestRunner
+
+class CPRobolectricTestRunner(testClass: Class<*>) : RobolectricTestRunner(testClass)
--- countrypicker/src/test/java/com/hbb20/countrypicker/CPRobolectricTestRunner.kt
+++
@@ -1,6 +0,0 @@
-package com.hbb20.countrypicker
-
-import org.robolectric.RobolectricTestRunner
-
-class CPRobolectricTestRunner(testClass: Class<*> ) : RobolectricTestRunner(testClass){
-}
